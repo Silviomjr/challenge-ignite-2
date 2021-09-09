939016a5ddde0ebe6c10e0ee550fab4e5a4c37d3
Silvio Manenti Junior <silviomjr@live.com>
2021-09-08 21:42:09 -0300

    feat: components separation (header, sidebar, content)

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,45 @@
-export function Content() {
-  // Complete aqui
+import {MovieCard} from './MovieCard';
+import {Header} from './Header';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+};
+
+interface GenreResponseProps {
+  id: number;
+  name: string;
+  title: string;
+};
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
 }
+
+export function Content({selectedGenre, movies}:ContentProps) {
+  return (
+    <div className="container">
+
+      <Header 
+        title={selectedGenre.title}
+      />
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie:any) => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
+
+// finalizar o component container
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface SideBarProps {
+  selectedGenredId: number;
+  onChangeSelectedGenreId: (id: number) => void;
+  genres: GenreResponseProps[];
+}
+
+export function SideBar({selectedGenredId, onChangeSelectedGenreId, genres}: SideBarProps) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => {onChangeSelectedGenreId(genre.id)}}
+            selected={selectedGenredId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
